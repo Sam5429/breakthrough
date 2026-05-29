e79ccfd525bddd1e1aea5497b2886e04531581ff
nizar_saidi <108678460+nizarsaidicoder@users.noreply.github.com>
2026-05-29 13:52:03 +0200

    refactor: improve code quality and adhere to Java conventions

--- src/Move.java
+++ src/Move.java
@@ -8,12 +8,14 @@ public class Move {
 		this.target = target;
 	}
 
+	// CRITIQUE : Même amateurisme ici. Tu découpes la chaîne par " - " et tu accèdes aux index 0 et 1 directement sans vérifier si le découpage a bien produit au moins 2 éléments. Un simple espace manquant dans l'entrée réseau et ton code explose en ArrayIndexOutOfBoundsException.
 	public Move(String move) {
 		String[] parts = move.split(" - ");
 		this.init = new Position(parts[0]);
 		this.target = new Position(parts[1]);
 	}
 
+	// CRITIQUE : Ne pas redéfinir equals() et hashCode() pour un objet-valeur (Value Object) comme Move en Java est une faute professionnelle. C'est à cause de cette flemme intellectuelle que tu as dû coder un 'moveEquals' privé très vilain directement dans la classe Bot.
 	@Override
 	public String toString() {
 		return (init.toString() + " - " + target.toString());
